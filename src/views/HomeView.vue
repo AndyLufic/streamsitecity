50023b235033202e392daff9e66ecd919005d215
AndyLufic <andreaslucic9@gmail.com>
2022-09-16 16:22:52 +0200

    test commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="col-12 text-center mt-5">
+      <h1 class="text-white pt-4 ">Recommended</h1>
+     </div>
+    <div class="container">
+      <div class="row my-5">
+        <div class="col-md-4 my-4">
+          <img src="img/avengers.jpg" alt="" class="w-100">
+          <h4 class="text-white my 4">Avengers</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
